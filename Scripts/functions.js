580d850cf3c2eb36a528a238f86255a39cc6bfad
AbdelwahedAB <ababdelwahed112@gmail.com>
2022-03-27 00:34:36 -1100

    Add basic functions

---
+++ Scripts/functions.js
@@ -0,0 +1,30 @@
+function add(a, b){
+    return a + b;
+}
+
+function substract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return Math.round(a/b * 100) / 100;
+}
+
+function operate(a, b, operator){
+    switch(operator){
+        case "+" :
+            return add(a, b);
+        case "*" :
+            return multiply(a, b);
+        case "-" :
+            return substract(a, b);
+        case "/" :
+            return divide(a, b);
+    }
+}
+
+export {add, substract, multiply, divide, operate}
---
+++ Scripts/main.js
@@ -0,0 +1,3 @@
+import {add, multiply, divide, substract, operate} from "./functions.js"
+
+console.log(operate(10, 3,"/"));
